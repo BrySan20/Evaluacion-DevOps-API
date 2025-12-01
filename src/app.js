97e6f6169ff09e1b8673dfef4d997dee4f26818f
BrySan20 <bryansanchez1680@gmail.com>
2025-11-30 18:31:31 -0600

    Test Blue-Green V2

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ let tasks = [
 app.get('/api/status', (req, res) => {
     res.json({ 
         status: 'ok', 
-        version: 'V1 (BLUE)', // Cambiar esto manualmente en cada deploy para comprobar versión actual
+        version: 'V2 (GREEN)', // Cambiar esto manualmente en cada deploy para comprobar versión actual
         uptime: process.uptime() 
     });
 });
